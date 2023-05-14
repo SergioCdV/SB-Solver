37cf4cde2d436eeaba07c15870d6e4aa82f1cfb5
SergioCdV <66524268+SergioCdV@users.noreply.github.com>
2023-05-14 16:14:55 +0200

    More test problems

---
+++ Code/SBOP V10/+Problems/@RossTest2/ControlFunction.m
@@ -0,0 +1,10 @@
+%% Project: Shape-based optimization for low-thrust transfers %%
+% Date: 07/02/2023
+
+%% Control function %% 
+% Function implementation of the control function as a dynamics residual
+
+function [u] = ControlFunction(obj, params, beta, t0, tf, t, s)
+    % Compute the control vector as a dynamics residual
+    u = s(4,:);
+end
